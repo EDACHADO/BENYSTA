0c1be1a0e8bf3a28741f6170c142a6064ff77921
kennyt4real.ak <kennyt4real.ak@gmail.com>
2026-07-25 03:52:21 +0100

    Update Scalar API UI title to BENYSTA MFB NIBSS NPS Integration
    The API reference UI title in the Scalar API mapping was changed from "NIBBS National Payment Stack (NPS) Integration API" to "BENYSTA MFB NIBSS National Payment Stack (NPS) Integration API" for improved clarity.

--- Benysta.EasyPay.Integration/Integration.WebApi/Program.cs
+++ Benysta.EasyPay.Integration/Integration.WebApi/Program.cs
@@ -39,7 +39,7 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference(options =>
     {
         options
-            .WithTitle("NIBBS National Payment Stack (NPS) Integration API")
+            .WithTitle("BENYSTA MFB NIBSS National Payment Stack (NPS) Integration API")
             .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
     });
 }
